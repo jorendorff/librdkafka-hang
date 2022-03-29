15de90ec76935da622f95fbf59da8e861e930648
Jason Orendorff <jorendorff@github.com>
2022-03-28 21:43:20 -0500

    tidying up

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
-use rdkafka::{
-    config::RDKafkaLogLevel,
-    consumer::{BaseConsumer, Consumer},
-    ClientConfig, Message, Offset, TopicPartitionList,
-};
-use std::{
-    env,
-    time::{Duration, Instant},
-};
+use std::env;
+use std::time::{Duration, Instant};
+
+use rdkafka::config::RDKafkaLogLevel;
+use rdkafka::consumer::{BaseConsumer, Consumer};
+use rdkafka::{ClientConfig, Message, Offset, TopicPartitionList};
 
 fn main() {
     let args = env::args().skip(1).collect::<Vec<String>>();
@@ -21,34 +18,31 @@ fn run_test(store_offsets: bool) {
         .set("group.id", "test")
         .set("bootstrap.servers", "localhost:29094")
         .set("enable.partition.eof", "false")
-        .set("enable.auto.offset.store", "false") // We will control the consumer's in-memory offset store
+        .set("enable.auto.offset.store", "false") // application will set consumer offsets explicitly
         .set("enable.auto.commit", "true") // same as default value: let librdkafka handle committing offsets
-        .set("debug", "consumer,cgrp,topic,fetch") // enable very verbose, low-level kafka logging
+        .set("debug", "consumer,cgrp,topic,fetch") // enable low-level rdkafka logging
         .set_log_level(RDKafkaLogLevel::Debug);
 
-    let topic = "test";
-    let partition = 0;
     let mut topic_partition = TopicPartitionList::new();
     topic_partition
-        .add_partition_offset(topic, partition, Offset::Beginning)
+        .add_partition_offset("test", 0, Offset::Beginning)
         .unwrap();
 
     let consumer: BaseConsumer = client_config.create().unwrap();
     consumer.assign(&topic_partition).unwrap();
 
     let start_time = Instant::now();
-
     while start_time.elapsed() < Duration::from_secs(3) {
-        if store_offsets {
-            topic_partition.set_all_offsets(Offset::Offset(0)).unwrap();
-            consumer.store_offsets(&topic_partition).unwrap();
-        }
-
         match consumer.poll(Duration::from_secs(1)) {
             Some(Ok(msg)) => {
                 let payload = msg.payload().unwrap();
-                let content = std::str::from_utf8(payload).unwrap().to_string();
+                let content = std::str::from_utf8(payload).unwrap();
                 eprintln!("message received: {:?}", content);
+                if store_offsets {
+                    let offset = Offset::Offset(msg.offset());
+                    topic_partition.set_all_offsets(offset).unwrap();
+                    consumer.store_offsets(&topic_partition).unwrap();
+                }
             }
             Some(Err(e)) => eprintln!("error polling for kafka message: {}", e),
             None => {}
